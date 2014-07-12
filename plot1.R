942105622daed475f298dbd83a221c2115b95cf8
Vlad-Ovidiu Lupu <vladovidiulupu@gmail.com>
2014-07-13 00:03:03 +0300

    Added plot1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+source("downloadAndSubsetData.R")
+data <- downloadAndSubsetData()
+
+png("plot1.png", width = 480, height = 480)
+with(data, hist(Global_active_power, col = "red", 
+                xlab = "Global Active Power (kilowatts)",
+                main = "Global Active Power"))
+dev.off()
