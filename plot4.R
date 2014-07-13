8d6f495a1173411d897966896c1b35878aee9790
Vlad-Ovidiu Lupu <vladovidiulupu@gmail.com>
2014-07-13 19:37:37 +0300

    Plot4 display changes

--- plot4.R
+++ plot4.R
@@ -3,13 +3,16 @@ data <- downloadAndSubsetData(verbose = TRUE)
 
 png("plot4.png", width = 480, height = 480)
 
-par(mfcol = c(2, 2))
+par(mfcol = c(2, 2), pch = 20)
 
 # plot at (1,1)
-with(data, plot(DateTime, Global_active_power,
-                type = "l",
+with(data, {
+    plot(DateTime, Global_active_power,
+                type = "n",
                 xlab = "",
-                ylab = "Global Active Power (kilowatts)"))
+                ylab = "Global Active Power")
+    points(DateTime, Global_active_power, pch = 20, cex = 0.1 ,type = "o")
+    })
 
 # plot at (2,1)
 with(data, { 
@@ -17,6 +20,7 @@ with(data, {
          type = "l",
          xlab = "",
          ylab = "Energy sub metering")
+    points(DateTime, Sub_metering_1, cex = 0.5)
     lines(DateTime, Sub_metering_2,
           col = "red")
     lines(DateTime, Sub_metering_3,
@@ -24,20 +28,35 @@ with(data, {
     legend("topright",
            c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
            lty = c(1, 1, 1),
-           lwd = c(1, 1, 1),
+           cex = 0.9,
+           bty = "n",
+           x.intersp = 0.9,
+           y.intersp = 1,
            col = c("black", "red", "blue"))
-})
+    })
 
 # plot at (1,2)
-with(data, plot(DateTime, Voltage,
+with(data, {
+    plot(DateTime, Voltage,
                 type = "l",
+                lwd = 0.5,
                 xlab = "datetime",
-                ylab = "Voltage"))
+                ylab = "Voltage")
+    #points(DateTime[plottedPoints], Voltage[plottedPoints], cex = 0.1)
+    })
 
 # plot at (2,2)
-with(data, plot(DateTime, Global_reactive_power,
-                type = "l",
+with(data, {
+    plot(DateTime, Global_reactive_power,
+                type = "n",
+                lwd = 0.2,
                 xlab = "datetime",
-                ylab = "Global_reactive_power"))
+                ylab = "Global_reactive_power")
+    points(DateTime, Global_reactive_power, type = "b", cex = 0.2, lwd = 0.2)
+    })
+
+set.seed(331)
+
+
 
 dev.off()
