64bc8c9516ec0993e3b4989002f9ed96ad15156c
Vlad-Ovidiu Lupu <vladovidiulupu@gmail.com>
2014-07-13 16:00:52 +0300

    Added plot4

---
+++ plot4.R
@@ -0,0 +1,43 @@
+source("downloadAndSubsetData.R")
+data <- downloadAndSubsetData(verbose = TRUE)
+
+png("plot4.png", width = 480, height = 480)
+
+par(mfcol = c(2, 2))
+
+# plot at (1,1)
+with(data, plot(DateTime, Global_active_power,
+                type = "l",
+                xlab = "",
+                ylab = "Global Active Power (kilowatts)"))
+
+# plot at (2,1)
+with(data, { 
+    plot(DateTime, Sub_metering_1,
+         type = "l",
+         xlab = "",
+         ylab = "Energy sub metering")
+    lines(DateTime, Sub_metering_2,
+          col = "red")
+    lines(DateTime, Sub_metering_3,
+          col = "blue")
+    legend("topright",
+           c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lty = c(1, 1, 1),
+           lwd = c(1, 1, 1),
+           col = c("black", "red", "blue"))
+})
+
+# plot at (1,2)
+with(data, plot(DateTime, Voltage,
+                type = "l",
+                xlab = "datetime",
+                ylab = "Voltage"))
+
+# plot at (2,2)
+with(data, plot(DateTime, Global_reactive_power,
+                type = "l",
+                xlab = "datetime",
+                ylab = "Global_reactive_power"))
+
+dev.off()
