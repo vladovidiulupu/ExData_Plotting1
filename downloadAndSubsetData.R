c48dfe173724f29c7dc7d449f9ca551cf5e0e8fc
Vlad-Ovidiu Lupu <vladovidiulupu@gmail.com>
2014-07-13 15:16:04 +0300

    Added verbose option

--- downloadAndSubsetData.R
+++ downloadAndSubsetData.R
@@ -1,4 +1,4 @@
-downloadAndSubsetData <- function () {
+downloadAndSubsetData <- function (verbose = FALSE) {
     
     #download the data if it is not available
     if (!file.exists("data/household_power_consumption.txt")) {
@@ -7,22 +7,32 @@ downloadAndSubsetData <- function () {
             dir.create("data")
     
         if (!file.exists("data/household_power_consumption.zip")) {
+            if (verbose) print("Downloading data...")
             fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-            download.file(fileUrl, destfile="./data/household_power_consumption.zip")
+            download.file(fileUrl, quiet = !verbose,
+                          destfile="./data/household_power_consumption.zip")
         }
         
+        if (verbose) print("Decompressing data...")
         unzip("data/household_power_consumption.zip", exdir = "data")
     }
     
-    #convert to date/time
+    if (verbose) print("Reading data...")
     fullDataset <- read.table("data/household_power_consumption.txt", header = TRUE,
-                              sep = ";", na.strings = "?")
-    fullDataset$Date <- as.Date(fullDataset$Date, format = "%d/%m/%Y")
-    fullDataset$Time <- strptime(fullDataset$Time, format = "%H:%M:%S")
+                              sep = ";", na.strings = "?", dec = ".")
     
-    #subset the data
-    date1 <- as.Date("2007-02-01", format = "%Y-%m-%d")
-    date2 <- as.Date("2007-02-02", format = "%Y-%m-%d")
-    requiredData <- subset(fullDataset, Date == date1 | Date == date2)
+    #subset the data to only include the 2 required days
+    subsettedData <- subset(fullDataset, Date == "1/2/2007" | Date == "2/2/2007")
+    
+    #convert from strings to POSIX time
+    pTimeColumn <- strptime(paste(subsettedData$Date, subsettedData$Time),
+                            format = "%d/%m/%Y %H:%M:%S",
+                            tz = "GMT")
+    
+    #remove the two old date/time columns and add the new one
+    dateTimeColumnIndices <- which(names(subsettedData) == "Time" | names(subsettedData) == "Date")
+    requiredData <- subsettedData[, -dateTimeColumnIndices]
+    requiredData <- cbind(pTimeColumn, requiredData)
+    names(requiredData)[1] = "DateTime"
     requiredData
 }
